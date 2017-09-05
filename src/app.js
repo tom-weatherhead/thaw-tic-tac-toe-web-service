c3374bed2d97be7342618c5bcabb7cd52b8968c0
Tom Weatherhead <tom_weatherhead@hotmail.com>
2017-09-04 22:00:40 -0600

    Optimized error reporting

--- src/app.js
+++ src/app.js
@@ -5,41 +5,24 @@
 'use strict';
 
 const gameEngine = require('thaw-tic-tac-toe-engine');
-const minMaxPly = gameEngine.minMaxPly;	// 1;
-const maxMaxPly = gameEngine.maxMaxPly;	// 6;
 
 const express = require('express');
 const app = express();
 
 const router = express.Router();				// eslint-disable-line new-cap
 
-const errorMessages = gameEngine.errorMessages;
-
 router.get('/:board([EXO]{9})/:maxPly([0-9]{1})', function (req, res) {
 	// Global replace in string: See https://stackoverflow.com/questions/38466499/how-to-replace-all-to-in-nodejs
 	const boardString = req.params.board.replace(/E/g, ' ');		// Replaces all 'E' with ' '.
 	const maxPly = parseInt(req.params.maxPly, 10);
 
-	if (maxPly < minMaxPly || maxPly > maxMaxPly) {
-		const message = errorMessages.maxPlyOutOfRange(maxPly, minMaxPly, maxMaxPly);
+	try {
+		const result = gameEngine.findBestMove(boardString, maxPly);
 
-		console.error(message);
-		res.status(400).send(message);
-	} else {
-
-		try {
-			const result = gameEngine.findBestMove(boardString, maxPly);
-
-			res.json(result);
-		} catch (error) {
-			// const message = errorMessages.gameEngineError(error.message);
-			const message = error.message;
-
-			console.error(error);
-			console.error(message);
-			// For a description of the Node.js Error class, see https://nodejs.org/api/errors.html#errors_class_error
-			res.status(500).send(message);
-		}
+		res.json(result);
+	} catch (error) {
+		// For a description of the Node.js Error class, see https://nodejs.org/api/errors.html#errors_class_error
+		res.status(500).send(error.message);
 	}
 });
 
