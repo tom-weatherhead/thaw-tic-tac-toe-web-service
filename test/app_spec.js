a2b6a733cc889c08788828753cb3201e880fcbf5
Tom Weatherhead <tom_weatherhead@hotmail.com>
2018-08-28 22:15:11 -0600

    Fixed unit tests

--- test/app_spec.js
+++ test/app_spec.js
@@ -20,25 +20,30 @@ chai.use(chaiHttp);
 
 describe('App', function () {
 	testDescriptors.forEach(testDescriptor => {
+		console.log('testDescriptor is', testDescriptor);
 		describe(testDescriptor.name, function () {
 			it('Rocks!', function (done) {
 				// Arrange
 				const url = '/tictactoe/' + testDescriptor.boardString.replace(/ /g, 'E') + '/' + testDescriptor.maxPly;
 
 				// Act
-				chai.request(app).get(url).end(function (error, result) {
+				//chai.request(app).get(url).end(function (error, result) {
+				chai.request(app).get(url).then(result => {
+					//console.log('result is', result);
+					console.log('result.status is', result.status);
+					console.log('result.text is', result.text);
 					// Assert
 
-					if (error) {
-						expect(testDescriptor.errorHandlingFunction).to.be.not.null;	// eslint-disable-line no-unused-expressions
-						// testDescriptor.errorHandlingFunction(gameEngine, expect, error);
-						testDescriptor.errorHandlingFunction(gameEngine, expect, result.error.text);
-					} else {
-						expect(error).to.be.null;										// eslint-disable-line no-unused-expressions
-						expect(result).to.be.not.null;									// eslint-disable-line no-unused-expressions
-						expect(result.body).to.be.not.null;								// eslint-disable-line no-unused-expressions
+					//expect(error).to.be.null;										// eslint-disable-line no-unused-expressions
+					expect(result).to.be.not.null;									// eslint-disable-line no-unused-expressions
+					expect(result.body).to.be.not.null;								// eslint-disable-line no-unused-expressions
+
+					if (result.status === 200) {
 						expect(testDescriptor.verificationFunction).to.be.not.null;		// eslint-disable-line no-unused-expressions
 						testDescriptor.verificationFunction(gameEngine, expect, result.body);
+					} else {
+						expect(testDescriptor.errorHandlingFunction).to.be.not.null;	// eslint-disable-line no-unused-expressions
+						testDescriptor.errorHandlingFunction(gameEngine, expect, result.text);
 					}
 
 					done();
